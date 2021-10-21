6a4e45580c09885bfd131b0cde80a443b5532682
Jay <jay@atliq.com>
2021-10-21 19:10:41 +0530

    SignIn SignUp screens UI

--- src/screens/Auth/SignInScreen.js
+++ src/screens/Auth/SignInScreen.js
@@ -12,25 +12,24 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { CircularProgress } from '@mui/material';
 import { Redirect } from 'react-router';
+import { isAuthenticated } from '../../Helpers/UserHelper';
 
 
 const theme = createTheme();
 
 export default function SignIn() {
-  const [name,setName]=useState("")
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
-  const [cPassword,setCPassword]=useState("")
   const [loading,setLoading]=useState(false)
   const [redirectToSign,setRedirectToSign]=useState(false)
   const handleSubmit =async (event) => {
     setLoading(true);
     event.preventDefault();
-    await fetch(`${process.env.REACT_APP_API_URL}/auth/sign-up`, {
+    await fetch(`${process.env.REACT_APP_API_URL}/auth/sign-in`, {
       method: "POST",
       headers:{'Content-type':'application/json'},
+      credentials:'include',
       body:JSON.stringify({
-        name,
         email,
         password
       })
@@ -42,11 +41,15 @@ export default function SignIn() {
           setLoading(false);
         }
         return response.json();
+      }).then((obj)=>{
+        localStorage.setItem("jwt", obj.jwt);
+        localStorage.setItem("user_id", obj.user_id);
       })
       .catch((err) => console.log(err));
+
   };
-  if (redirectToSign) {
-    return <Redirect to={`/signIn`}></Redirect>;
+  if (isAuthenticated() ||  redirectToSign) {
+    return <Redirect to={`/`}></Redirect>;
   }
   return (
     <ThemeProvider theme={theme}>
@@ -70,20 +73,9 @@ export default function SignIn() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign Up
+            Sign In
           </Typography>
           <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              onChange={(e)=>setName(e.target.value)}
-              value={name}
-              id="name"
-              label="Name"
-              name="name"
-              autoFocus
-            />
             <TextField
               margin="normal"
               required
@@ -109,25 +101,13 @@ export default function SignIn() {
               id="password"
               autoComplete="current-password"
             />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              value={cPassword}
-              onChange={(e)=>setCPassword(e.target.value)}
-              name="cpassword"
-              label="Confirm Password"
-              type="cpassword"
-              id="cpassword"
-              autoComplete="current-password"
-            />
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign Up
+              Sign In
             </Button>
           </Box>
         </Box>
